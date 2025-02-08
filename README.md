8b323911882136009e3f8c2c59e08cccd4f3b56c
Bram Borggreve <beeman@beeman.nl>
2025-02-08 12:41:53 +0000

    refactor: remove vite-react styling

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # metaplex-umi-starters
+
+A bunch of starter projects for <a href="https://github.com/metaplex-foundation/umi">Metaplex Umi</a>.
+
+## Projects
+
+### vite-react
+
+```shell
+pnpx create-solana-dapp@latest -t beeman/metaplex-umi-starters/vite-react
+```
--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+
 import { umi } from './umi.ts'
 import { BlockhashWithExpiryBlockHeight } from '@metaplex-foundation/umi'
 
@@ -7,19 +7,16 @@ function App() {
   const [blockhash, setBlockhash] = useState<BlockhashWithExpiryBlockHeight | null>(null)
 
   async function onClick() {
-    umi.rpc.getLatestBlockhash()
-      .then(res => {
-        console.log(res)
-        setBlockhash(res)
-      })
+    umi.rpc.getLatestBlockhash().then((res) => {
+      console.log(res)
+      setBlockhash(res)
+    })
   }
 
   return (
-    <div className="card">
-      <button onClick={onClick}>
-        Get Latest Blockhash
-      </button>
-      <pre>{blockhash ? JSON.stringify(blockhash, null, 2) : 'Loading...'}</pre>
+    <div>
+      <button onClick={onClick}>Get Latest Blockhash</button>
+      <pre>{blockhash ? JSON.stringify(blockhash, null, 2) : ''}</pre>
     </div>
   )
 }
